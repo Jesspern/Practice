f2fd7e290c319d37a292c74eb6f0ba0d340571c0
Jesspern <90327793+Jesspern@users.noreply.github.com>
2023-07-05 22:30:54 +0300

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab1;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello world!");
+		Student stud = new Student("Andrey", "Bobovich", "Pavlovich", 8, 213, 2021, 'Б', "C#");
+		Console.WriteLine(stud.ToString());
+	}
+}
---
+++ Lab1/Lab1/Student.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+	public class Student:
+		IEquatable<Student>
+	{
+		private string _name;
+		private string _surname;
+		private string _patronymic;
+
+		private int _number_faculty;
+		private int _number_group;
+		private int _year_entry;
+		private char _type_education;
+
+		private string _type_practice;
+
+		public Student(string? name, string? surname, string? patronymic, int number_faculty, int number_group,
+			int year_entry, char type_education, string? type_practice)
+		{
+			try
+			{
+				_name ??= name;
+			}
+			catch (ArgumentNullException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			try
+			{
+				_surname ??= surname;
+			}
+			catch (ArgumentNullException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			try
+			{
+				_patronymic ??= patronymic;
+			}
+			catch (ArgumentNullException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			try
+			{
+				_type_practice ??= type_practice;
+			}
+			catch (ArgumentNullException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			_number_faculty = number_faculty;
+			_number_group = number_group;
+			_year_entry = year_entry;
+			_type_education = type_education;
+		}
+
+		public string Name
+		{
+			get { return _name; }
+
+			set { _name = value; }
+		}
+
+		public string Surname
+		{
+			get { return _surname; }
+			set { _surname = value; }
+		}
+
+		public string Patronymic
+		{
+			get { return _patronymic; }
+			set { _patronymic = value; }
+		}
+		public string Type_practice
+		{
+			get { return _type_practice; }
+			set { _type_practice = value; }
+		}
+
+		public int GetNumberCourse()
+		{
+			return 2023 - _year_entry;
+		}
+
+		public override string ToString()
+		{
+			return $"ФИО: {_surname} {_name} {_patronymic}, Учебная группа: М{_number_faculty}О-{_number_group}{_type_education}-{_year_entry}, Курс практики: {_type_practice}";
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(_name, _surname, _patronymic, _number_faculty, _number_group, _year_entry, _type_education, _type_practice);
+		}
+
+		public bool Equals(Student? obj)
+		{
+			if (obj == null) return false;
+
+			if (obj is Student std)
+			{
+				return Equals(std);
+			} else
+			{
+				return false;
+			}
+		}
+	}
+}
